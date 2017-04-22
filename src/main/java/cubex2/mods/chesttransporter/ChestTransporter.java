65acb9bc830cb47740a7e6690ae74a0c785cc741
cubex2 <firebeast-breuer@web.de>
2017-04-22 11:45:09 +0200

    Bump version

--- src/main/java/cubex2/mods/chesttransporter/ChestTransporter.java
+++ src/main/java/cubex2/mods/chesttransporter/ChestTransporter.java
@@ -23,7 +23,7 @@ import java.util.EnumMap;
 
 import static cubex2.mods.chesttransporter.ChestTransporter.ID;
 
-@Mod(modid = ID, name = "Chest Transporter", version = "2.5.13")
+@Mod(modid = ID, name = "Chest Transporter", version = "2.5.14")
 public class ChestTransporter
 {
     @Mod.Instance("ChestTransporter")
